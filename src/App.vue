a7b405408b8f293f4eae864079f77e8bd8597d17
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-08-18 23:18:02 +0300

    06-dynamic-routing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link :to="{ name: 'Home' }">Events</router-link> |
-    <router-link :to="{ name: 'About' }">About</router-link>
+    <router-link to="/">Events</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
